8d12c13c92b26da0f7ab63e39aba07f69fc867a0
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2026-06-11 12:53:14 +0300

    feat: init repo

---
+++ packages/hyperellipse/src/index.ts
@@ -0,0 +1,2 @@
+/** biome-ignore-all lint/performance/noBarrelFile: Barrel file is used to export the register function */
+export { registerHyperellipse } from "./register";
---
+++ packages/hyperellipse/src/register.ts
@@ -0,0 +1,3 @@
+export const registerHyperellipse = () => {
+  console.log("Hyperellipse registered");
+};
